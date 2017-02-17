1e70201f48ba2a63646b68e3c984e3218dfa88e0
Karsten Jakobsen <kj@evercall.dk>
2017-02-17 11:34:49 +0100

    Added git branch variable

--- circleci-helpers/upload.bash
+++ circleci-helpers/upload.bash
@@ -1,27 +1,27 @@
 #!/bin/bash
-ENVIRONMENT=$1
+BRANCH=$1
 
 echo -n "Working dir: "
 pwd
 
 function production {
-  echo "Running prd.."
+  echo "Running production.."
   aws s3 sync build/ s3://www.artrunde.com --delete --exclude "*.png"
   aws s3 sync build/ s3://assets.artrunde.com --exclude "*" --include "images/*"	
 }
 
 function development {
-  echo "Running dev..."
+  echo "Running development..."
   aws s3 sync build/ s3://dev-www.artrunde.com --delete --exclude "*.png"
   aws s3 sync build/ s3://dev-assets.artrunde.com --exclude "*" --include "images/*"	
 }
 
-if [[ $ENVIRONMENT == "prd" ]]
+if [[ $BRANCH == "master" ]]
 then
   production
 fi
 
-if [[ $ENVIRONMENT == "dev" ]]
+if [[ $BRANCH == "development" ]]
 then
   development
 fi
